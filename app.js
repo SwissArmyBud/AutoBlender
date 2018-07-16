dcc2d2219ce7732d00a988473ba7c9b65783c063
Andrew Porter <andrew@mbay.io>
2018-07-15 19:08:19 -0700

    SQUASH

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log("Client disconnected!");
     console.log(socket.id);
-    fs.remove(__dirname + "/Website/{uploads,downloads}/" + socketID + ".mp3", err => {
+    fs.remove(__dirname + "/Website/{uploads,downloads}/" + socket.id + ".mp3", err => {
       if (err) return console.error(err);
       console.log('Cleanup ok!');
     });
