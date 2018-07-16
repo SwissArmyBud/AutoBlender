677bf49d964539057b736e360758aaafa8f474a1
Andrew Porter <andrew@mbay.io>
2018-07-16 14:52:03 -0700

    Better cleanup

--- Website/helpers/socketBinder.js
+++ Website/helpers/socketBinder.js
@@ -9,14 +9,20 @@ module.exports = function(io, applicationPath){
     socket.on('disconnect', function(){
       console.log("Client disconnected!");
       console.log(socket.id);
-      fs.remove(applicationPath + "/Website/uploads/" + socket.id + "*", function(err){
-        if (err) return console.error(err);
-        console.log('Cleanup ok!');
-      });
-      fs.remove(applicationPath + "/Website/downloads/" + socket.id + "*", function(err){
-        if (err) return console.error(err);
-        console.log('Cleanup ok!');
-      });
+      var uploadExtensions = [".mp3"];
+      for(var upExt in uploadExtensions){
+        fs.remove(applicationPath + "/Website/uploads/" + socket.id + upExt, function(err){
+          if (err) return console.error(err);
+          console.log(upExt + ' cleanup ok!');
+        });
+      }
+      var downloadExtensions = [".brs", ".bts", ".vol", ".mel"];
+      for(var dnExt in downloadExtensions){
+        fs.remove(applicationPath + "/Website/downloads/" + socket.id + dnExt, function(err){
+          if (err) return console.error(err);
+          console.log(dnExt + ' cleanup ok!');
+        });
+      }
     });
   });
 };
