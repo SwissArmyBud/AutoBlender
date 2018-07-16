c1e7562e7622b8de62c948d96bd09b8a5c02863c
Andrew Porter <andrew@mbay.io>
2018-07-15 18:49:01 -0700

    Rename clientID to socketID

--- Website/routes/basicRoutes.js
+++ Website/routes/basicRoutes.js
@@ -8,25 +8,24 @@ module.exports = function(io, applicationPath){
 
   router.post('/upload', function(req, res) {
     var fstream;
-    var clientID;
+    var socketID;
     // When BusBoy catches the file upload, stream it to the server upload area
     req.busboy.on('file', function (fieldname, file, filename) {
         console.log("Uploading: " + filename);
         // Set path and init stream
-        fstream = fs.createWriteStream(applicationPath + '/uploads/' + filename);
+        fstream = fs.createWriteStream(applicationPath + '/uploads/' + socketID);
         file.pipe(fstream);
         // Handle file upload finishing
         fstream.on('close', function () {
-            io.sockets.to(clientID).emit("uploadStatus", {status: "::DONE::"});
+            io.sockets.to(socketID).emit("uploadStatus", {status: "::DONE::"});
             console.log("Upload Finished of " + filename);
         });
     });
     req.busboy.on('field', function(fieldname, val) {
-      console.log('Field [' + fieldname + ']: value: ' + val);
       if(fieldname == "socketID"){
-        clientID = val;
-        console.log("Client ID provided: " + clientID);
-        io.sockets.to(clientID).emit("uploadStatus", {status: "--> I SEE YOU <--"});
+        socketID = val;
+        console.log("Socket ID provided: " + socketID);
+        io.sockets.to(socketID).emit("uploadStatus", {status: "--> I SEE YOU <--"});
       }
     });
     req.busboy.on('finish', function() {
