e507b64e1976e56fd2493d91755a4cf0b5d4ddac
Andrew Porter <andrew@mbay.io>
2018-07-16 11:20:04 -0700

    Update test python script for longer (>1s) execution times

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("TEST OK!")
+import time
+
+
+print("--> TEST START")
+for i in range(4)
+    print("--> Val: " + str(i+1))
+    time.sleep(1)   # Delays for 5 seconds. You can also use a float value.
+print("--> TEST OK!")
