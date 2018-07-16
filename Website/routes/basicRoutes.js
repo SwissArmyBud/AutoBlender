aa04613dac7d811e6eaa8b73ddc246d66c6fff28
Andrew Porter <andrew@mbay.io>
2018-07-15 18:57:48 -0700

    Setup file upload copy to socketID for tracking

--- Website/routes/basicRoutes.js
+++ Website/routes/basicRoutes.js
@@ -11,15 +11,20 @@ module.exports = function(io, applicationPath){
     var socketID;
     // When BusBoy catches the file upload, stream it to the server upload area
     req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("Uploading: " + filename);
-        // Set path and init stream
-        fstream = fs.createWriteStream(applicationPath + '/uploads/' + socketID);
-        file.pipe(fstream);
-        // Handle file upload finishing
-        fstream.on('close', function () {
-            io.sockets.to(socketID).emit("uploadStatus", {status: "::DONE::"});
-            console.log("Upload Finished of " + filename);
+      console.log("Uploading: " + filename);
+      // Set path and init stream
+      var basePath = applicationPath + '/uploads/';
+      fstream = fs.createWriteStream(basePath + filename);
+      file.pipe(fstream);
+      // Handle file upload finishing
+      fstream.on('close', function () {
+        // Async with callbacks:
+        fs.copy(basePath + filename, basePath + socketID + ".mp3", function(err){
+          if (err) return console.error(err);
+          io.sockets.to(socketID).emit("uploadStatus", {status: "::DONE::"});
+          console.log("Upload Finished of " + filename);
         });
+      });
     });
     req.busboy.on('field', function(fieldname, val) {
       if(fieldname == "socketID"){
