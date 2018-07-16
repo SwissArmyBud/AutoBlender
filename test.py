13e4dc687659357e726bc6bb628162f2a594369e
Andrew Porter <andrew@mbay.io>
2018-07-16 11:22:17 -0700

    Python syntax SQUASH

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import time
 
 
 print("--> TEST START")
-for i in range(4)
-    print("--> Val: " + str(i+1))
+for i in range(1,5):
+    print("--> Val: " + str(i))
     time.sleep(1)   # Delays for 5 seconds. You can also use a float value.
 print("--> TEST OK!")
