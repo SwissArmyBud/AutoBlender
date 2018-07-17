f0996b2a0f7c10b684c59959eb38d3e2cb43ea4c
Andrew Porter <andrew@mbay.io>
2018-07-16 19:54:56 -0700

    Prep to run blender scene generation

--- Website/helpers/pythonRunner.js
+++ Website/helpers/pythonRunner.js
@@ -34,15 +34,39 @@ module.exports = {
       }
     });
   },
-  startBlenderEngine: function(io, socketID, cb){
+  startBlenderEngine: function(io, path, socketID, cb){
     console.log("Starting blender engine for socketID: " + socketID);
-    // TODO - Start blender scene construction
-    io.sockets.to(socketID).emit("uploadStatus", {
-      status: "::BRE",
-      stdout: "test bufferString"
+    // blender -b -P path/my_script.py --
+    // Spawn an UNBUFFERED Python3 child for audio analysis
+    var scriptPath = "./Website/downloads/" + socketID + ".brs";
+    var renderEngine = child("blender", ["-b", "-P", scriptPath, "--", socketID, 6], options={
+      cwd: path,
+      env: unbufferedPythonENV
     });
-    if(cb && typeof(cb)=="function"){
-      cb(0);
-    }
+    // Grab output and send back to client
+    renderEngine.stdout.on('data', function(buffer){
+      var bufferString = buffer.toString();
+      io.sockets.to(socketID).emit("uploadStatus", {
+        status: "::BRE",
+        stdout: bufferString
+      });
+      console.log('stdout:');
+      console.log(bufferString);
+    });
+    // Error to cosole for debug
+    renderEngine.stderr.on('data',  function(buffer){
+      console.log('stderr:');
+      console.log(buffer.toString());
+    });
+    // Fire callback on done, with exit code
+    renderEngine.on('close',  function(code){
+      console.log(socketID + 'child process exited with code:' + parseInt(code));
+      io.sockets.to(socketID).emit("uploadStatus", {status: "::BRD"});
+      if(cb && typeof(cb)=="function"){
+        cb(code);
+      }
+    });
+
+
   }
 };
